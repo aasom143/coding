18c0190a91b161b995e27b44b0421283f6e67754
aasom143 <35426854+aasom143@users.noreply.github.com>
2019-01-28 23:51:12 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define mod 1000000007
+#define int long long int
+#define ff first
+#define ss second
+void fast(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+// Geetha Govindam
+//cin >> ws;
+//string st;
+//getline(cin,st); 
+//scanf(" %[^\n]s,s);
+main()
+{
+	int t=10;
+	fast();t=1;
+	cin>>t;
+	while(t--){
+
+	}
+}
